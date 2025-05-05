2e2b67620b25ae887342105ec0581e081c2d8916
CptMusthy <mustafamemis@gmail.com>
2025-05-05 11:59:19 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,18 +5,14 @@ function setLanguage(lang) {
 
 function loadLanguage(lang) {
   fetch(`lang/${lang}.json`)
-    .then(res => {
-      if (!res.ok) throw new Error("Dil dosyası bulunamadı");
-      return res.json();
-    })
+    .then(res => res.json())
     .then(data => {
       document.querySelectorAll('[data-key]').forEach(el => {
         const key = el.getAttribute('data-key');
         if (data[key]) el.textContent = data[key];
       });
       if (data.title) document.title = data.title;
-    })
-    .catch(err => console.error("Dil yükleme hatası:", err));
+    });
 }
 
 document.addEventListener('DOMContentLoaded', () => {
